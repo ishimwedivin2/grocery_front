e42e537fbf5ae4fc0800dec7cf23d6969905a848
Ishimwe Divin <ishimdivin2019@gmail.com>
2024-12-22 11:22:27 +0200

    modified: AboutUsBody and Contact pages

--- src/components/AboutUsBody.jsx
+++ src/components/AboutUsBody.jsx
@@ -8,30 +8,30 @@ const AboutUsBody = () => {
         <div className="about-container">
           <h2>About Us</h2>
           <p className="vision">
-            Our mission is to provide top-quality products while striving to reduce our environmental impact. We believe in innovation, sustainability, and commitment to creating a better future for the planet.
+            Our mission is to provide top-quality products
           </p>
           <div className="team-section">
             <h3>Meet Our Team</h3>
             <div className="team-members">
               <div className="team-member">
-                <h4>Ashimwe Cedrick</h4>
+                <h4>Ishimwe Divin</h4>
                 <p>CEO & Founder</p>
                 <p>
-                  Cedrick is the visionary behind our company, with a passion for sustainable business practices and innovation.
+                  Divin is the visionary behind our company.
                 </p>
               </div>
               <div className="team-member">
-                <h4>Igabamahoro Jehovanis</h4>
-                <p>Chief Product Officer</p>
+                <h4>Kayigamba Aime</h4>
+                <p>Chief products</p>
                 <p>
-                  Jehovanis leads the product development team, ensuring every product meets the highest standards of quality.
+                  Aime leads the product development team, ensuring every product meets the highest standards of quality.
                 </p>
               </div>
               <div className="team-member">
-                <h4>Bucyukundi Jonathan</h4>
-                <p>Head of Sustainability</p>
+                <h4>Ashimwe Cedrick</h4>
+                <p>Product Manager</p>
                 <p>
-                  Jonathan is committed to environmental sustainability, making sure our processes are eco-friendly and impactful.
+                  Cedrick is committed to environmental sustainability
                 </p>
               </div>
             </div>
